a43a3615d220ba7ad3fbd15a76b818a1627e432f
afatoga <afatoga@gmail.com>
2024-08-14 15:52:10 +0200

    Batch length endpoint

--- test/integration/index.itest.ts
+++ test/integration/index.itest.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from '@jest/globals'
 import { Client } from '../../src/client/index.js'
 
 describe('Authenticate endpoint', () => {
-  test('success, returns data', async () => {
+  test('success, returns user data', async () => {
     const testClient = new Client({
      clientId: process.env.TEST_CLIENT_ID ?? '',
      clientSecret: process.env.TEST_CLIENT_SECRET ?? '',
@@ -17,6 +17,26 @@ describe('Authenticate endpoint', () => {
       id: expect.any(String),
       token: expect.any(Object),
       remainingMonthlyRequests: expect.any(Number),
+    })
+  })
+})
+
+describe('Batch length endpoint', () => {
+  test('success, returns existing batch count', async () => {
+    const testClient = new Client({
+      clientId: process.env.TEST_CLIENT_ID ?? '',
+      clientSecret: process.env.TEST_CLIENT_SECRET ?? '',
+      // userEmail: process.env.TEST_USER_EMAIL ?? '',
+      // userPassword: process.env.TEST_USER_PASSWORD ?? '',
+     })
+    const response = await testClient.endpoints.batch.length()
+    expect(response).toEqual({
+      data: expect.objectContaining({
+        length: expect.any(Number)
+      }),
+      userData: {
+        remainingRequests: expect.any(String),
+      }
     })
   })
 })
